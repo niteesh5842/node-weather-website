880ada96fc0ca97e5090ace5d2dad1181833447b
niteesh pandey <niteesh.pandeyjswbpsl@gmail.com>
2021-10-18 10:31:44 +0530

    name changed

--- src/app.js
+++ src/app.js
@@ -91,7 +91,7 @@ app.get("/products", (req, res) => {
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Niteesh Pandey",
     errorMessage: "Help article not found.",
   });
 });
@@ -99,7 +99,7 @@ app.get("/help/*", (req, res) => {
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Niteesh Pandey",
     errorMessage: "Page not found.",
   });
 });
